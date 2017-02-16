7ffc53496e880c36fd6db10be7584f0e55b10a9a
ybogdanov <dante4645@yandex.ru>
2017-02-16 11:06:49 +0400

    загрузка, фильтрация и сохранение файлов

---
+++ PlanExam/PlanExam/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using PlanExam.Utils;
+
+namespace PlanExam.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase upload)
+        {
+            if (upload == null) return RedirectToAction("Index");
+            // получаем имя файла
+            string fileName = System.IO.Path.GetFileName(upload.FileName);
+            if (!HttpPostedFileBaseExtensions.IsImage(upload))
+            {
+
+                return View("Index");
+            }
+            // сохраняем файл в папку Files в проекте
+            Logger.Info("Выполняется сохранение файла {0} на сервере ...", upload.FileName);
+            try
+            {
+                upload.SaveAs(Server.MapPath("~/Files/" + fileName));
+            }
+            catch (Exception e)
+            {
+                Logger.Error(e, "При сохранении файла на сервере произошла ошибка.");
+                return View("Index");
+            }
+            Logger.Info("Файл успешно сохранен.");
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ PlanExam/PlanExam/Utils/HttpPostedFileBaseExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Web;
+
+namespace PlanExam.Utils
+{
+    public static class HttpPostedFileBaseExtensions
+    {
+        public const int ImageMinimumBytes = 512;
+
+        //честно нагуглено на stackoverflow
+
+        public static bool IsImage(this HttpPostedFileBase postedFile)
+        {
+            //-------------------------------------------
+            //  Check the image mime types
+            //-------------------------------------------
+            if (postedFile.ContentType.ToLower() != "image/jpg" &&
+                        postedFile.ContentType.ToLower() != "image/jpeg" &&
+                        postedFile.ContentType.ToLower() != "image/pjpeg" &&
+                        postedFile.ContentType.ToLower() != "image/gif" &&
+                        postedFile.ContentType.ToLower() != "image/x-png" &&
+                        postedFile.ContentType.ToLower() != "image/png" &&
+                        postedFile.ContentType.ToLower() != "application/pdf")
+            {
+                return false;
+            }
+
+            //-------------------------------------------
+            //  Check the image extension
+            //-------------------------------------------
+            var extension = Path.GetExtension(postedFile.FileName);
+            if (extension != null && (extension.ToLower() != ".jpg" && extension.ToLower() != ".png" && extension.ToLower() != ".gif" && extension.ToLower() != ".jpeg" && extension.ToLower() != ".pdf") )
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
